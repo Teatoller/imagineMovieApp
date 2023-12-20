d932277032cb273a40ba62a1ad708a7b66ed102c
stevenennis <seotieno@safaricom.co.ke>
2023-12-20 09:51:01 +0300

    ft(responsive css): make movi ui screen responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+// import './App.css'
 import { MovieDetails } from './view/movie/MovieDetails'
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className="container" style={{border: "4px solid tomator"}}>
        <h3>Imagine Movie App</h3>
        <MovieDetails />
       </div>
